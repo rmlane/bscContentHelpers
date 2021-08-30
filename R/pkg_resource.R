574da9b3ded38552b08959c6911d88b2baf67155
Rachel Lane <rlane7@uic.edu>
2021-08-30 13:22:31 -0500

    updates pkg_resource() to allow flexible package reference

--- R/pkg_resource.R
+++ R/pkg_resource.R
@@ -1,14 +1,15 @@
 #' Fetch a Resource from the Current Package
 #'
 #' @param ... The search string for the desired resource
+#' @param package The name of the package
 #'
 #' @return The path to the resource
 #' @export
 #'
 #' @examples pkg_resource("rmd_files/custom.css")
-pkg_resource <- function(...) {
+pkg_resource <- function(..., package = "bscContentHelpers") {
 	# look for a match
-	f = system.file(..., package = "bscContentHelpers")
+	f = system.file(..., package = package)
 
 	# if nothing is found, return NULL
 	if (f == "") {f = NULL}
